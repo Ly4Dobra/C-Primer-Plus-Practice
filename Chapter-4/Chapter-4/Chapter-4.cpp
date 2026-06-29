fdad13443ea66a625e6a7bba275dc6da002c30fc
sskobelev <sskobelev@rnd.avancore.ru>
2026-06-29 15:08:15 +0300

    exercise 1-2

--- Chapter-4/Chapter-4/Chapter-4.cpp
+++ Chapter-4/Chapter-4/Chapter-4.cpp
@@ -1,20 +1,34 @@
-﻿// Chapter-4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <string>
+void exercise_1();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    exercise_1();
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+void exercise_1()
+{
+    std::cout << "What is your first name? ";
+
+    std::string firstName;
+    std::getline(std::cin, firstName);
+
+    std::cout << "What is your last name? ";
+    std::string lastName;
+    std::getline(std::cin, lastName);
+
+    std::cout << "What letter grade do you deserve? ";
+    char letterDeserve;
+    std::cin >> letterDeserve;
+
+    std::cout << "What is your age? ";
+    int age;
+    std::cin >> age;
+
+    std::cout << "Name: " << lastName << ", " << firstName << std::endl;
+    std::cout << "Grade: " << char(letterDeserve + 1) << std::endl;
+    std::cout << "Age: " << age;
+
+}
